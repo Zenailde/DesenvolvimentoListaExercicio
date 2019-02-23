9db66655b7bfcb5b56ea54de6e7c16d8880dcdf2
Zenailde <43657182+Zenailde@users.noreply.github.com>
2019-02-22 21:18:54 -0300

    Add files via upload

---
+++ Fatura.java
@@ -0,0 +1,28 @@
+public class Fatura {
+
+    private String codigo;
+    private String descricao;
+    private Integer qtd;
+    private double preco;
+
+
+    public Fatura(String codigo, String descricao, Integer qnt, double preco){
+        super();
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.qtd = qnt;
+        this.preco = preco;
+    }
+
+
+    public double totalFaturado(){
+
+        double total = qtd * preco;
+        return total;
+    }
+
+
+}
+
+
+
